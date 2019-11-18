a26d8ce75a9def1e873d723c746bb55407c6c947
Darius Weber <darius.weber@posteo.de>
2019-11-18 17:56:35 +0100

    Add some code to trigger GitHub Actions

--- src/main/java/de/weber/githubpackagestest2/ExceptionHelper.java
+++ src/main/java/de/weber/githubpackagestest2/ExceptionHelper.java
@@ -4,6 +4,12 @@ public class ExceptionHelper
 {
     private ExceptionHelper() {}
 
+    /**
+     * Throws an exception if if the passed value is null
+     * @param obj checked if null
+     * @param argumentName name of the variable
+     * @throws ArgumentNullException
+     */
     public static void ThrowIfNull(Object obj, String argumentName) throws ArgumentNullException
     {
         if (obj == null) {
